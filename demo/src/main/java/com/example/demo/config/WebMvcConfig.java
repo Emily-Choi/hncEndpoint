c00053235bc59b855637cf73c827374953c76568
user.name <@EMCHOI-OUT>
2021-02-19 14:02:35 +0900

    운영반영

--- demo/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -1,18 +1,21 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
+	@Value("${file.upload-dir}")
+    String fDir;
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/documents/**")
       
 //                .addResourceLocations("file:/DATA/video/");     } //리눅스 root에서 시작하는 폴더 경로
-        .addResourceLocations("file:///D:/documents/");
+        .addResourceLocations("file:" + fDir);
     	//윈도우일 경우 아래처럼 사용하면 된다.
     	//addResourceLocations("file:///D:/DATA/video/");
 }
